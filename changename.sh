be3ab1c28c238fb6d664f4b96714645b153af07e
Jenniferrmuniz <Jennifer.r.muniz@gmail.com>
2019-09-09 18:00:59 -0400

    gogog

---
+++ changename.sh
@@ -0,0 +1,17 @@
+git filter-branch --env-filter '
+
+OLD_EMAIL="Jen@Jennifers-MacBook-Pro-3.local"
+CORRECT_NAME="Jenniferrmuniz"
+CORRECT_EMAIL="jennifer.r.muniz@gmail.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
